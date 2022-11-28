3b770426fc6c172f1c02414d601c96b395631d9d
larryro <371767072@qq.com>
2022-11-28 17:39:04 +0800

    fix bug

--- test/testAxios.js
+++ test/testAxios.js
@@ -4,21 +4,20 @@ console.log(`Hello Node.js v${process.versions.node}!`)
 
 const axios = require('axios')
 
-const url =
-  'https://api.thetadrop.com/pack/pack_d7h6zjr8180fdkymr4kn0nj1zr5.json?nft_id=nft_gqghctwjchy0cihskqy0c3wjvna3'
+// const url =
+//   'https://api.thetadrop.com/type/type_q6kexwknrap7xzdhk8x2t9b5447.json?nft_id=nft_iiepzyvy3i5803ywnb59h8cuu0pv'
 
 async function main() {
-  const res = await axios({
-    method: 'get',
-    url,
-    timeout: 10000,
-    responseType: 'json',
-    responseEncoding: 'utf8',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept-Encoding': 'gzip, deflate, br'
-    }
-  })
+  const option = {
+    url: 'https://api.thetadrop.com/type/type_f3s3nre856scgsbcbsphnfu2guy.json?nft_id=nft_urfuk33h7ygrjb7tp7s5qecyz89z',
+    timeout: 3000,
+    method: 'get'
+    // responseType: 'json'
+    // responseEncoding: 'utf8',
+  }
+
+  const res = await axios(option)
   console.log(res)
+  console.log(res.data)
 }
 main()
